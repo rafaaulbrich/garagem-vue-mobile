2bc877af7f57703138ea34c3c7468ad258237a70
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-07-23 11:13:49 -0300

    Ajustes nos designs das telas

---
+++ src/components/templates/LogoTitle.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+<template>
+  <div class="logo">
+    <img src="@/assets/logo.png" alt="Garagem Logo" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  padding-left: 3rem;
+  gap: 1rem;
+  align-items: center;
+}
+
+.logo img {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+</style>
---
+++ src/components/templates/medium/MediumAside.vue
@@ -0,0 +1,9 @@
+<script setup>
+import LogoTitle from '@/components/templates/LogoTitle.vue';
+</script>
+
+<template>
+    <logo-title class="pl-0" />
+</template>
+
+<style scoped></style>
